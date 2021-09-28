745828f88c53c63cb55ab571b2a8c9710f5b6fb6
Mihail Nazarov <msnazarow@gmail.com>
2021-09-28 16:58:30 +0300

    Initial Commit

---
+++ DynamicShapes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicShapes
+//
+//  Created by out-nazarov2-ms on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
